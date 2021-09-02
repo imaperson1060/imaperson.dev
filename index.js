08ab5c4a86b53ccbaf74076e40cbd0f8f6b38c9b
Ari Meisels <me@arimeisels.com>
2021-09-02 16:59:34 -0400

    Remove Troll Endpoint
    Who knows what this was for? It's useless now!

--- index.js
+++ index.js
@@ -73,21 +73,6 @@ app.post("/restart/", (req, res) => {
 });
 
 
-app.get("/troll/", cors(), (req, res) => {
-    app.set("json spaces", 0);
-    res.json([{ "enabled": Math.round(Math.random() * 100) != 1 }]);
-    app.set("json spaces", 4);
-
-    let date_ob = new Date();
-    let date = ("0" + date_ob.getDate()).slice(-2);
-    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-    let year = date_ob.getFullYear();
-    let hours = date_ob.getHours();
-    let minutes = date_ob.getMinutes();
-    let seconds = date_ob.getSeconds();
-    console.log(year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
-});
-
 server.listen(process.env.PORT, () => {
     console.log("API Ready!");
 });
