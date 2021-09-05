64b44387cf02a0b6ade9fa875cfd2221077b56f3
Ari Meisels <me@arimeisels.com>
2021-09-05 01:03:20 -0400

    Bringing Shortcutter into DJS v13 :D
    Update to slash commands yayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy

--- discord/shortcutter/bot.js
+++ discord/shortcutter/bot.js
@@ -1,45 +1,61 @@
-const { Client, MessageAttachment } = require("discord.js"),
+const { Client, MessageEmbed } = require("discord.js"),
       client = new Client({ "intents": ["GUILDS", "GUILD_MESSAGES"] });
 
 client.login(process.env.SHORTCUTTERBOT);
 
 client.on("ready", () => {
-	client.user.setActivity("Shortcutter | >help", { type: "PLAYING" });
+	client.user.setActivity("Shortcutter | >help");
+
+	console.log("Shortcutter ready!");
 });
 
-client.on("messageCreate", (message) => {
-    if (message.content.startsWith(">shortcut")) {
-        if (message.mentions.members.first()) {
-            message.reply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.",  files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/users/${message.mentions.members.first().id}\r\nIconFile=${message.guild.iconURL()}\r\nIconIndex=0`, "utf-8"), name: `${message.mentions.members.first().user.username}.url` } ] });
-        } else if (message.mentions.channels.first()) {
-            message.reply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.", files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/channels/${message.guild.id}/${message.mentions.channels.first().id}\r\nIconFile=${message.guild.iconURL()}\r\nIconIndex=0`, "utf-8"), name: `${message.mentions.channels.first().name}.url` } ] });
-        } else {
-			message.reply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.", files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/channels/${message.guild.id}/${message.channel.id}\r\nIconFile=${message.guild.iconURL()}\r\nIconIndex=0`, "utf-8"), name: `${message.channel.name}.url` } ] });
+client.on("interactionCreate", interaction => {
+	if (!interaction.isCommand()) return;
+
+	var options = interaction.options._hoistedOptions;
+
+	switch (interaction.commandName) {
+		case "shortcut":
+			(async function () {
+				await interaction.deferReply({ ephemeral: false });
+
+				if (findObjectByKey(options, "name", "user")) {
+		            return await interaction.editReply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.",  files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/users/${findObjectByKey(options, "name", "user").user.id}\r\nIconFile=https://cdn.discordapp.com/icons/${findObjectByKey(options, "name", "user").member.guild.id}/${findObjectByKey(options, "name", "user").member.guild.icon}.webp\r\nIconIndex=0`, "utf-8"), name: `${findObjectByKey(options, "name", "user").user.username}.url` } ] });
+        		} else if (findObjectByKey(options, "name", "channel")) {
+		            return await interaction.editReply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.", files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/channels/${findObjectByKey(options, "name", "channel").channel.guildId}/${findObjectByKey(options, "name", "channel").value}\r\nIconFile=https://cdn.discordapp.com/icons/${findObjectByKey(options, "name", "channel").channel.guild.id}/${findObjectByKey(options, "name", "channel").channel.guild.icon}.webp\r\nIconIndex=0`, "utf-8"), name: `${findObjectByKey(options, "name", "channel").channel.name}.url` } ] });
+        		} else {
+					return await interaction.editReply({ content: "**WARNING:**\nBrowsers will block the shortcut because internet shortcuts are commonly used for scams.", files: [ { attachment: Buffer.from(`[InternetShortcut]\r\nURL=discord://discordapp.com/channels/${interaction.member.guild.id}/${interaction.channelId}\r\nIconFile=https://cdn.discordapp.com/icons/${interaction.member.guild.id}/${interaction.member.guild.icon}.webp\r\nIconIndex=0`, "utf-8"), name: `${client.channels.cache.get(interaction.channelId).name}.url` } ] });
+        		}
+			})();
+
+			break;
+		case "help":
+			(async function () {
+				await interaction.deferReply({ ephemeral: false });
+
+				const help = new MessageEmbed()
+                    .setColor("#7289DA")
+                    .setAuthor("Shortcutter", "https://cdn.discordapp.com/app-icons/838967702872916000/3904dbced0ef3498599f443848a0fa29.png?size=256", "https://discord.com/api/oauth2/authorize?client_id=838967702872916000&permissions=2147483648&scope=applications.commands%20bot")
+                    .setDescription("This bot was created by imaperson#1060!")
+                    .addFields(
+                        { name: "Use the bot", value: "Type *>shortcut* in any channel the bot can send messages in to use.\nYou can also mention a user or a different channel to link to there." },
+						{ name: "Invite it here:", value: "[https://discord.com/api/oauth2/authorize?client_id=838967702872916000&permissions=2147483648&scope=applications.commands%20bot](https://discord.com/api/oauth2/authorize?client_id=838967702872916000&permissions=2147483648&scope=applications.commands%20bot)" }
+                    )
+                    .setTimestamp()
+                    .setFooter("© imaperson#1060", "https://api.arimeisels.com/favicon.png");
+
+				interaction.editReply({ embeds: [ help ] });
+			})();
+
+			break;
+	}
+});
+
+function findObjectByKey(array, key, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i][key] === value) {
+            return array[i];
         }
-	}
-
-	if (message.content == ">help") {
-		message.reply({
-			embeds: [{
-				"color": "#7289DA",
-				"author": {
-					"name": "Shortcutter",
-					"icon_url": "https://avatars3.githubusercontent.com/u/68653653?s=48&v=4"
-				},
-				"title": "This bot was created by imaperson!",
-				"fields": [{
-					"name": "Use the bot:",
-					"value": "Type *>shortcut* in any channel the bot can send messages in to use.\nYou can also mention a user or a different channel to link to there."
-				},
-				{
-					"name": "Invite it here:",
-					"value": "[https://discord.com/api/oauth2/authorize?client_id=838967702872916000&permissions=34816&scope=bot](https://discord.com/api/oauth2/authorize?client_id=838967702872916000&permissions=34816&scope=bot)"
-				}],
-				"timestamp": new Date(),
-				"footer": {
-					"text": "© imaperson"
-				}
-			}]
-		});
-	}
-});
+    }
+    return null;
+}
