d6fad3d2601e2d1f49843ef4072c54208945e8be
Ari Meisels <me@arimeisels.com>
2021-09-10 18:50:51 -0400

    better account management

--- accounts.js
+++ accounts.js
@@ -2,17 +2,13 @@ module.exports = (app, cors, mail, md5, query, rateLimit) => {
     const accountLimiter = rateLimit({
         windowMs: 15 * 60 * 1000, // 1 minute
         max: 1,
-        message: { "success": false, "message": "Only one account can be created every 15 minutes." }
+        message: { "success": false, "message": "ratelimit" }
     });
 
     app.get("/signup/:name/:username/:email/:password", accountLimiter, cors(), async (req, res) => {
-        if (req.params.username.length > 32) {
-            return res.json({ "success": false, "message": `Username "${req.params.username}" is too long.` });
-        }
-
-        if (("_! @#$%^&*,".indexOf(req.params.username) != -1) || req.params.username.startsWith("-")) {
-            return res.json({ "success": false, "message": `Username "${req.params.username}" contains invalid characters.` });
-        }
+        if (req.params.username.length > 32) return res.json({ "success": false, "message": "long" });
+        if (("_! @#$%^&*,".indexOf(req.params.username) != -1) || req.params.username.startsWith("-")) return res.json({ "success": false, "message": "invalidchar" });
+        if (["api", "5beam", "sql"].indexOf(req.params.username) != -1) return res.json({ success: false, message: "reserved" });
         
         function capitalize(str) {
             return str.replace(/\w\S*/g, function(txt) {
@@ -20,48 +16,48 @@ module.exports = (app, cors, mail, md5, query, rateLimit) => {
             });
         }
         
-        if ((await query("SELECT * FROM `accounts` WHERE username=?", [req.params.username]))[0]) return res.json({ success: false, message: `Username "${req.params.username}" has been taken.` })
-        if ((await query("SELECT * FROM `accounts` WHERE email=?", [req.params.email]))[0]) return res.json({ success: false, message: `Email "${req.params.email}" is already in use.`});
+        if ((await query("SELECT * FROM `accounts` WHERE username=?", [req.params.username]))[0]) return res.json({ success: false, message: "taken" })
+        if ((await query("SELECT * FROM `accounts` WHERE email=?", [req.params.email]))[0]) return res.json({ success: false, message: "emailused" });
         
         var timestamp = Math.round(new Date().getTime() / 1000);
         
-        var highestId = (await query("SELECT MAX(id) AS highestId FROM `accounts` LIMIT 1")).highestId || 0;
+        var highestId = (await query("SELECT MAX(id) AS highestId FROM `accounts` LIMIT 1"))[0].highestId || 0;
         
         await mail(req.params.email, "Welcome", `<h1>Hey, ${capitalize(req.params.name)}! Thanks for creating an account on my website!</h1> <h2>If you have any questions, feel free to <a href="mailto:me@arimeisels.com">shoot me an email</a>!</h3> <style>* { font-family: sans-serif }</style>`);
         
-        await query("INSERT INTO `accounts` VALUES (?,?,?,?,?,?)", [highestId + 1, req.params.username, capitalize(req.params.name), req.params.email, md5(req.params.password), timestamp]);
+        await query("INSERT INTO `accounts`(`id`, `username`, `name`, `email`, `password`, `timestamp`) VALUES (?,?,?,?,?,?)", [highestId + 1, req.params.username, capitalize(req.params.name), req.params.email, md5(req.params.password), timestamp]);
         
         require("child_process").execSync(`pktriot tunnel http add --domain ${req.params.username}.arimeisels.com --destination localhost --http 8000 --letsencrypt && taskkill.exe /f /im pktriot.exe`);
 
-        res.json({ success: true, message: `Account "${req.params.username}" has been created.` });
+        res.json({ success: true });
     });
 
     app.get("/login/:username/:password", cors(), async (req, res) => {
         const info = (await query("SELECT * FROM `accounts` WHERE username=?", [req.params.username]))[0];
 
-        if (!info) return res.json({ success: false, message: `Account ${req.params.username} does not exist.` });
+        if (!info) return res.json({ success: false, message: "noaccount" });
         
         if ((req.params.password == info.password) || (md5(req.params.password) == info.password)) {
             res.json({ success: true });
         } else {
-            res.json({ success: false, message: `The password is incorrect.` });
+            res.json({ success: false, message: "wrongpass" });
         }
     });
 
     app.get("/forgot/:email/", cors(), async (req, res) => {
         const info = (await query("SELECT * FROM `accounts` WHERE email=?", [req.params.email]))[0];
         
-        if (!info) return res.json({ success: false, message: `There is no account linked to ${req.params.email}.` });
+        if (!info) return res.json({ success: false, message: "noaccountlinked" });
 
-        await mail(info.email, "Reset Password", `<h1>Hi, ${info.name}!</h1> <h2>Click <a href="https://api.arimeisels.com/reset/${info.username}/${md5(info.username + info.password)}/">here</a> to reset your password!</h2> <h3>If you did not request to have your password reset, please ignore this email.</h3> <style>* { font-family: sans-serif }</style>`);
+        await mail(info.email, "Reset Password", `<h1>Hi, ${info.name}!</h1> <h2>Click <a href="https://api.arimeisels.com/reset.html?u=${info.username}&c=${md5(info.username + info.password)}">here</a> to reset your password!</h2> <h3>If you did not request to have your password reset, please ignore this email.</h3> <style>* { font-family: sans-serif }</style>`);
 
         res.json({ "success": true });
     });
 
     app.get("/reset/:username/:code/", cors(), async (req, res) => {
-        const info = (await query("SELECT * FROM `accounts` WHERE username?", [req.params.username]))[0];
+        const info = (await query("SELECT * FROM `accounts` WHERE username=?", [req.params.username]))[0];
 
-        if (!info) return res.json({ success: false, message: `Account ${req.params.username} does not exist.` });
+        if (!info) return res.json({ success: false, message: "noaccount" });
 
         if (req.params.code == md5(info.username + info.password)) {
             const newPassword = md5(Math.floor(Date.now() / 1000) + info.password);
@@ -70,9 +66,9 @@ module.exports = (app, cors, mail, md5, query, rateLimit) => {
 
             await query("UPDATE `accounts` SET password=? WHERE username=?", [md5(newPassword), req.params.username]);
 
-            res.json({ success: true, message: newPassword });
+            res.json({ success: true, password: newPassword });
         } else {
-            res.json({ success: false, message: `The access code is incorrect.`});
+            res.json({ success: false, message: "wrongcode" });
         }
     });
 }
