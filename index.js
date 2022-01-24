963dc7c59f53abf200e66a503dd24750735b506b
Ari Meisels <me@arimeisels.com>
2022-01-24 02:50:56 -0500

    Wow I'm dumb

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set("json spaces", 4);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
-app.get("/", function (req, res, next) {
+app.all("*", function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
