fc0c01dce77b944d5dca545377e38b5d4d28047c
Ari Meisels <me@arimeisels.com>
2022-01-24 20:14:53 -0500

    Commit #150 - The Commit Page Update
    Buncha new things:
    * Change card-header to card-title and resize card to make it look better
    * Add date navigation (1 week intervals)
    * Store commits in database for faster access and less ratelimiting

--- socket/github/commits.js
+++ socket/github/commits.js
@@ -1,7 +1,20 @@
 import moment from "moment";
+import mysql from "mysql";
 import fetch from "node-fetch";
+import util from "util";
+
+var mysqlLogin = JSON.parse(process.env.MYSQL);
+mysqlLogin = Object.assign(mysqlLogin, { database: "github" });
+var database = mysql.createPool(mysqlLogin);
+var query = util.promisify(database.query).bind(database);
+
 export default async (io, socket, args) => {
-    var commits = await fetch(`https://api.github.com/repos/imaperson1060/arimeisels.com/commits?per_page=100&since=${moment().subtract(7, "d").toISOString()}`, {
+    if (args[0] && ((args[0].since && !args[0].until) || (!args[0].since && args[0].until) || !moment(args[0].since).isValid() || !moment(args[0].until).isValid())) return socket.emit("github-commits", { success: false, message: "invalidargs", args });
+
+    var since = args[0] ? moment(args[0].since).toISOString() : moment().subtract(7, "d").toISOString();
+    var until = args[0] ? moment(args[0].until).add(1, "d").toISOString() : moment();
+
+    var commits = await fetch(`https://api.github.com/repos/imaperson1060/arimeisels.com/commits?per_page=100&since=${since}&until=${until}`, {
         headers: {
             Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}`
         }
@@ -10,11 +23,17 @@ export default async (io, socket, args) => {
     commits = await commits.json();
 
     for await (const x of commits) {
-        x.stats = (await (await fetch(`https://api.github.com/repos/imaperson1060/arimeisels.com/commits/${x.sha}`, {
-            headers: {
-                Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}`
-            }
-        })).json()).stats;
+        if (!(await query("SELECT * FROM `commits` WHERE id=?", [ x.sha ]))[0]) {
+            x.stats = (await (await fetch(`https://api.github.com/repos/imaperson1060/arimeisels.com/commits/${x.sha}`, {
+                headers: {
+                    Authorization: `Basic ${btoa(`imaperson1060:${process.env.GITHUB_TOKEN}`)}`
+                }
+            })).json()).stats;
+
+            await query("INSERT INTO `commits`(`id`, `name`, `author`, `stats`, `date`) VALUES (?,?,?,?,?)", [ x.sha, encodeURIComponent(x.commit.message), encodeURIComponent(x.commit.author.name), JSON.stringify(x.stats), x.commit.author.date ]);
+        } else {
+            x.stats = JSON.parse((await query("SELECT * FROM `commits` WHERE id=?", [ x.sha ]))[0].stats);
+        }
     }
 
     socket.emit("github-commits", { commits, args });
