2d747dc32b0f738970f38a9cf0c61fe3e5f71ea9
Ari Meisels <me@arimeisels.com>
2022-01-25 20:02:14 -0500

    PersonBot is backkkkkkkk

--- discord/personbot/commands/shorten.js
+++ discord/personbot/commands/shorten.js
@@ -1,65 +1,76 @@
+import fetch from "node-fetch";
+import mysql from "mysql";
+import { RateLimiterMemory } from "rate-limiter-flexible";
+import util from "util";
+
+var mysqlLogin = JSON.parse(process.env.MYSQL);
+mysqlLogin = Object.assign(mysqlLogin, { database: "short" });
+var database = mysql.createPool(mysqlLogin);
+var query = util.promisify(database.query).bind(database);
+
+async function deleteExpired() {
+    const expired = await query("SELECT * from `urls` WHERE expiration<?", [Math.round(new Date().getTime() / 1000)]);
+    expired.forEach(async (x) => {
+        await query("DELETE FROM `urls` WHERE id=?", [x.id]);
+    });
+}
+
+const rateLimiter = new RateLimiterMemory({ points: 1, duration: 60 });
+
 export default async function (client, interaction, options) {
-    function findObjectByKey(array, key, value) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i][key] === value) {
-                return array[i];
-            }
-        }
-        return null;
-    }
-
-    const mysqlLogin = JSON.parse(process.env.MYSQL);
-    var database = require("mysql").createPool(mysqlLogin);
-    var query = require("util").promisify(database.query).bind(database);
-
-    const isReachable = require("is-reachable");
-
-
     await interaction.deferReply({ ephemeral: false });
 
-    var url = ((findObjectByKey(options, "name", "url").value.startsWith("http://") ? findObjectByKey(options, "name", "url").value.slice(7) : (findObjectByKey(options, "name", "url").value.startsWith("https://") ? findObjectByKey(options, "name", "url").value.slice(8) : findObjectByKey(options, "name", "url").value)));
-    var expiration = findObjectByKey(options, "name", "expiration").value;
-    var id = (findObjectByKey(options, "name", "id") ? findObjectByKey(options, "name", "id").value : undefined);
-    var domain = findObjectByKey(options, "name", "domain").value;
-    var expirations = {
-        "12h": 43200,
-        "1d": 86400,
-        "1w": 604800,
-        "1m": 2419200,
-        "6m": 14515200,
-        "1y": 31536000,
-        "∞": 2147483647
-    }
-        
-    var https = await isReachable(`https://${decodeURIComponent(url)}`);
-    var http = await isReachable(`http://${decodeURIComponent(url)}`);
+    deleteExpired();
 
-    if (!https && !http) {
-        return await interaction.editReply("That website kinda doesn't exist, or it cannot be detected by me. Sorry!");
-    }
+    try {
+        await rateLimiter.consume(interaction.user.id);
 
-    if (id) {
-        if ((await query("SELECT * FROM `short` WHERE `id`=?", [id]))[0]) {
-            await interaction.editReply("That ID is already in use!");
-            return;
-        } else {
-            await query("INSERT INTO `short` (`id`, `longurl`, `domain`, `username`, `expiration`) VALUES (?, ?, ?, ?, ?)", [id, (https ? `https://${decodeURIComponent(url)}` : `http://${decodeURIComponent(url)}`), domain, "Guest", Math.round(new Date().getTime() / 1000) + expirations[expiration]]);
-            await interaction.editReply(`Shortened to https://ariurls.${domain}/${id} !`);
-            return;
+        var url = ((options.find(x => x.name == "url").value.startsWith("http://") ? options.find(x => x.name == "url").value.slice(7) : (options.find(x => x.name == "url").value.startsWith("https://") ? options.find(x => x.name == "url").value.slice(8) : options.find(x => x.name == "url").value)));
+        var expiration = options.find(x => x.name == "expiration").value;
+        var id = (options.find(x => x.name == "id") ? options.find(x => x.name == "id").value : undefined);
+        var domain = options.find(x => x.name == "domain").value;
+        var expirations = {
+            "12h": 43200,
+            "1d": 86400,
+            "1w": 604800,
+            "1m": 2419200,
+            "6m": 14515200,
+            "1y": 31536000,
+            "∞": 2147483647
         }
-    } else {
-        var characters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9"];
+            
+        const urls = await (await fetch("https://bot.arimeisels.com/url", { method: "POST", body: decodeURIComponent(url) })).json();
 
-        (async function generateURL() {
-            var randomString = characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)];
-                
-            if (await query("SELECT * FROM `short` WHERE `id`=?", [randomString])[0]) {
-                generateURL();
-            } else {
-                await query("INSERT INTO `short` (`id`, `longurl`, `domain`, `username`, `expiration`) VALUES (?, ?, ?, ?, ?)", [randomString, (https ? `https://${decodeURIComponent(url)}` : `http://${decodeURIComponent(url)}`), domain, "Guest", Math.round(new Date().getTime() / 1000) + expirations[expiration]]);
-                await interaction.editReply(`Shortened to https://ariurls.${domain}/${randomString} !`);
+        if (!urls.https && !urls.http) {
+            return await interaction.editReply("That website kinda doesn't exist, or it cannot be detected by me. Sorry!");
+        }
+
+        if (id) {
+            const result = (await query("SELECT * FROM `urls` WHERE name=?", [id]))[0];
+            if (result) {
+                await interaction.editReply("That ID is already in use!");
                 return;
+            } else {
+                await query("ALTER TABLE `urls` AUTO_INCREMENT=?", [(await query("SELECT MAX(`id`) AS max FROM `urls`"))[0].max]);
+                await query("INSERT INTO `urls`(`name`, `longurl`, `domain`, `creator`, `expiration`) VALUES (?,?,?,?,?)", [id, urls.https ? `https://${decodeURIComponent(url)}` : `http://${decodeURIComponent(url)}`, domain, "Guest", Math.round(new Date().getTime() / 1000) + expirations[expiration]]);
+
+                return await interaction.editReply(`Shortened to https://ariurls.${domain}/${id} !`);
             }
-        })();
+        } else {
+            var characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+            (async function generateURL() {
+                var randomString = characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)] + characters[Math.floor(Math.random() * characters.length)];
+
+                if ((await query("SELECT * FROM `urls` WHERE name=?", [randomString]))[0]) return generateURL();
+
+                await query("ALTER TABLE `urls` AUTO_INCREMENT=?", [(await query("SELECT MAX(`id`) AS max FROM `urls`"))[0].max]);
+                await query("INSERT INTO `urls`(`name`, `longurl`, `domain`, `creator`, `expiration`) VALUES (?,?,?,?,?)", [randomString, urls.https ? `https://${decodeURIComponent(url)}` : `http://${decodeURIComponent(url)}`, domain, "Guest", Math.round(new Date().getTime() / 1000) + expirations[expiration]]);
+
+                return await interaction.editReply(`Shortened to https://ariurls.${domain}/${randomString} !`);
+            })();
+        }
+    } catch (rejRes) {
+        return await interaction.editReply(`You're being ratelimited! Try again in *${Math.round(rejRes.msBeforeNext / 1000)}* seconds.`);
     }
 }
--- discord/personbot/commands/ytdownload.js
+++ discord/personbot/commands/ytdownload.js
@@ -1,27 +1,73 @@
-export default async function (client, interaction, options) {
-    const isReachable = require("is-reachable");
-    const ytdl = require("ytdl-core");
+import mysql from "mysql";
+import urlexists from "url-exists";
+import util from "util";
+import ytdl from "ytdl-core";
+import ytpl from "ytpl";
+import ytsr from "ytsr";
 
-    await interaction.deferReply({ ephemeral: true });
-    
-    function getId(url) {
-        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
-        var match = url.match(regExp);
-        return (match && match[7].length == 11) ? match[7] : false;
-    }        
+var urlExists = util.promisify(urlexists).bind(urlexists);
 
-    var id = getId(findObjectByKey(options, "name", "video").value);
+var mysqlLogin = JSON.parse(process.env.MYSQL);
+mysqlLogin = Object.assign(mysqlLogin, { database: "yt" });
+var database = mysql.createPool(mysqlLogin);
+var query = util.promisify(database.query).bind(database);
 
-    if (!(await isReachable(`https://i.ytimg.com/vi/${id}/hqdefault.jpg`))) return await interaction.editReply("The video submitted does not exist.");
+const yt = { dl: ytdl, pl: ytpl, sr: ytsr };
 
-    const videoInfo = await ytdl.getInfo(id);
+async function getVideoDetails(id, cookie) {
+    const db = (await query("SELECT * FROM `videos` WHERE id=?", [id]))[0];
+    if (db && db.timestamp + 21600 >= Math.round(new Date().getTime() / 1000)) {
+        return { success: true, formats: JSON.parse(decodeURIComponent(db.formats)), author: decodeURIComponent(db.author), title: decodeURIComponent(db.title), description: decodeURIComponent(db.description), thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` };
+    }
+
+    var videoInfo;
+    if (cookie) {
+        try {
+            videoInfo = await yt.dl.getInfo(id, { requestOptions: { headers: { cookie } } });
+        } catch (e) {
+            return { success: false, message: "invalid_cookie", code: 403 };
+        }
+    } else {
+        try {
+            videoInfo = await yt.dl.getInfo(id);
+        } catch (e) {
+            if (e.statusCode == 410) return { success: false, message: "cookies_required", code: 410 };
+            return { success: false, e };
+        }
+    }
     
     var hd;
-    try { hd = findObjectByKey(videoInfo.formats, "itag", 22).url; } catch (e) {}
-    var sd = findObjectByKey(videoInfo.formats, "itag", 18).url;
-    var audio = findObjectByKey(videoInfo.formats, "itag", 140).url;
+    try { hd = videoInfo.formats.find(x => x.itag == 22).url; } catch (e) {}
+    var sd = videoInfo.formats.find(x => x.itag == 18).url;
+    var audio = videoInfo.formats.find(x => x.itag == 140).url;
+    var formats = { hd, sd, audio };
+    
+    if ((await query("SELECT * FROM `videos` WHERE video=?", [id]))[0]) {
+        if (!videoInfo.videoDetails.isPrivate) await query("UPDATE `videos` SET author=?, title=?, description=?, formats=? WHERE video=?", [encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description), JSON.stringify(formats), id]);
+    } else {
+        await query("ALTER TABLE `videos` AUTO_INCREMENT=?", [(await query("SELECT MAX(`id`) AS max FROM `videos`"))[0].max]);
+        if (!videoInfo.videoDetails.isPrivate) await query("INSERT INTO `videos`(`video`, `author`, `title`, `description`, `formats`) VALUES (?,?,?,?,?)", [id, encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description) || "", JSON.stringify(formats)])
+    }
 
-    if (findObjectByKey(options, "name", "format").value == "mp4") {
+    return { success: true, formats: formats, info: { author: videoInfo.videoDetails.author.name, title: videoInfo.videoDetails.title, description: videoInfo.videoDetails.description, thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` } };
+}
+
+function getId(url) {
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match && match[7].length == 11) ? match[7] : false;
+}
+
+export default async function (client, interaction, options) {
+    await interaction.deferReply({ ephemeral: true });
+    
+    var id = getId(options.find(x => x.name == "video").value);
+
+    if (!(await urlExists(`https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${id}`))) return await interaction.editReply("The video submitted does not exist.");
+
+    const videoInfo = await getVideoDetails(id);
+
+    if (options.find(x => x.name == "format").value == "mp4") {
         const videoEmbed = {
             color: "#0099ff",
             author: {
@@ -29,22 +75,22 @@ export default async function (client, interaction, options) {
                 iconURL: client.user.displayAvatarURL(),
                 url: "https://discord.com/api/oauth2/authorize?client_id=882471379910426664&permissions=2147483648&scope=bot%20applications.commands"
             },
-            title: `${videoInfo.videoDetails.title} by ${videoInfo.videoDetails.author.name}`,
+            title: `${videoInfo.info.title} by ${videoInfo.info.author}`,
             url: `https://youtu.be/${id}`,
-            description: videoInfo.videoDetails.description,
+            description: videoInfo.info.description,
             fields: [
                 {
                     name: '\u200B',
                     value: '\u200B'
                 },
                 {
-                    name: "Video URL",
-                    value: `[Download the video](${hd ? hd : sd}) (link expires in 6 hours)`
+                    name: "Download link",
+                    value: `[Download the video](${videoInfo.formats.hd ? videoInfo.formats.hd : videoInfo.formats.sd}) (link expires in 6 hours)`
                 }
             ],
             timestamp: new Date(),
             footer: {
-                icon_url: "https://api.arimeisels.com/favicon.png",
+                icon_url: "https://arimeisels.com/favicon.png",
                 text: "PersonBot by imaperson#1060"
             }
         }
@@ -58,35 +104,26 @@ export default async function (client, interaction, options) {
                 iconURL: client.user.displayAvatarURL(),
                 url: "https://discord.com/api/oauth2/authorize?client_id=882471379910426664&permissions=2147483648&scope=bot%20applications.commands"
             },
-            title: `${videoInfo.videoDetails.title} by ${videoInfo.videoDetails.author.name}`,
+            title: `${videoInfo.info.title} by ${videoInfo.info.author}`,
             url: `https://youtu.be/${id}`,
-            description: videoInfo.videoDetails.description,
+            description: videoInfo.info.description,
             fields: [
                 {
                     name: '\u200B',
                     value: '\u200B'
                 },
                 {
-                    name: "Video URL",
-                    value: `[Download the audio](${audio}) (link expires in 6 hours)`
+                    name: "Download link",
+                    value: `[Download the audio](${videoInfo.formats.audio}) (link expires in 6 hours)`
                 }
             ],
             timestamp: new Date(),
             footer: {
-                icon_url: "https://api.arimeisels.com/favicon.png",
+                icon_url: "https://arimeisels.com/favicon.png",
                 text: "PersonBot by imaperson#1060"
             }
         }
 
         await interaction.editReply({ embeds: [ audioEmbed ] });
-    }
-
-    function findObjectByKey(array, key, value) {
-        for (var i = 0; i < array.length; i++) {
-            if (array[i][key] === value) {
-                return array[i];
-            }
-        }
-        return null;
     }
 }
