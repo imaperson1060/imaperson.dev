1c94650f83d7760fdd2fe54370c7ed6c247d8cda
Ari Meisels <me@arimeisels.com>
2022-01-27 00:07:07 -0500

    Add length variable to YouTube downloader

--- discord/personbot/commands/ytstream.js
+++ discord/personbot/commands/ytstream.js
@@ -18,7 +18,7 @@ const yt = { dl: ytdl, pl: ytpl, sr: ytsr };
 async function getVideoDetails(id, cookie) {
     const db = (await query("SELECT * FROM `videos` WHERE id=?", [id]))[0];
     if (db && db.timestamp + 21600 >= Math.round(new Date().getTime() / 1000)) {
-        return { success: true, formats: JSON.parse(decodeURIComponent(db.formats)), author: decodeURIComponent(db.author), title: decodeURIComponent(db.title), description: decodeURIComponent(db.description), thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` };
+        return { success: true, formats: JSON.parse(decodeURIComponent(db.formats)), author: decodeURIComponent(db.author), title: decodeURIComponent(db.title), description: decodeURIComponent(db.description), length: db.length, thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` };
     }
 
     var videoInfo;
@@ -44,13 +44,13 @@ async function getVideoDetails(id, cookie) {
     var formats = { hd, sd, audio };
     
     if ((await query("SELECT * FROM `videos` WHERE video=?", [id]))[0]) {
-        if (!videoInfo.videoDetails.isPrivate) await query("UPDATE `videos` SET author=?, title=?, description=?, formats=? WHERE video=?", [encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description), JSON.stringify(formats), id]);
+        if (!videoInfo.videoDetails.isPrivate) await query("UPDATE `videos` SET author=?, title=?, description=?, length=?, formats=? WHERE video=?", [encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description) || "", videoInfo.videoDetails.lengthSeconds, JSON.stringify(formats), id]);
     } else {
         await query("ALTER TABLE `videos` AUTO_INCREMENT=?", [(await query("SELECT MAX(`id`) AS max FROM `videos`"))[0].max]);
-        if (!videoInfo.videoDetails.isPrivate) await query("INSERT INTO `videos`(`video`, `author`, `title`, `description`, `formats`) VALUES (?,?,?,?,?)", [id, encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description) || "", JSON.stringify(formats)])
+        if (!videoInfo.videoDetails.isPrivate) await query("INSERT INTO `videos`(`video`, `author`, `title`, `description`, `formats`) VALUES (?,?,?,?,?)", [id, encodeURIComponent(videoInfo.videoDetails.author.name), encodeURIComponent(videoInfo.videoDetails.title), encodeURIComponent(videoInfo.videoDetails.description) || "", videoInfo.videoDetails.lengthSeconds, JSON.stringify(formats)])
     }
 
-    return { success: true, formats: formats, info: { author: videoInfo.videoDetails.author.name, title: videoInfo.videoDetails.title, description: videoInfo.videoDetails.description, thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` } };
+    return { success: true, formats: formats, info: { author: videoInfo.videoDetails.author.name, title: videoInfo.videoDetails.title, description: videoInfo.videoDetails.description, length: videoInfo.videoDetails.lengthSeconds, thumbnail: `https://i.ytimg.com/vi/${id}/mqdefault.jpg` } };
 }
 
 function getId(url) {
@@ -83,9 +83,9 @@ export default async function (client, interaction, options) {
 
     await interaction.editReply(`Playing "${videoInfo.info.title}" in :loud_sound: ${channel.name}`);
 
-    player.on(AudioPlayerStatus.Idle, () => {
+    setTimeout(() => {
         player.stop();
         subscription.unsubscribe();
         connection.destroy();
-    });
+    }, videoInfo.info.length * 1000);
 }
