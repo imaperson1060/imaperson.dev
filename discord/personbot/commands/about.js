a96d85ecb4e67227432ab4e795be2092ba60cefa
Ari Meisels <me@arimeisels.com>
2022-01-27 16:14:03 -0500

    Update PersonBot permissions

--- discord/personbot/commands/about.js
+++ discord/personbot/commands/about.js
@@ -1,5 +1,5 @@
 export default async function (client, interaction, options) {
     await interaction.deferReply({ ephemeral: true });
 
-    await interaction.editReply(`**PERSONBOT** by imaperson#1060\nInvite: https://discord.com/api/oauth2/authorize?client_id=882471379910426664&permissions=2147483648&scope=bot%20applications.commands`);
+    await interaction.editReply(`**PERSONBOT** by imaperson#1060\nInvite: https://discord.com/api/oauth2/authorize?client_id=882471379910426664&permissions=2150632448&scope=bot%20applications.commands`);
 }
