419c6a54dcb7ae300123b55cab4a6a04f290c7bc
Ari Meisels <me@arimeisels.com>
2022-02-16 20:41:37 -0500

    Playlist Downloader

---
+++ socket/yt/playlist.js
@@ -0,0 +1,11 @@
+import ytdl from "ytdl-core";
+import ytpl from "ytpl";
+import ytsr from "ytsr";
+
+const yt = { dl: ytdl, pl: ytpl, sr: ytsr };
+
+export default async (io, socket, args) => {
+    if (!args[0] || !args[0].id) return socket.emit("yt-getPlaylist", { success: false, message: "invalidargs", args });
+    
+    socket.emit("yt-getPlaylist", { success: true, playlist: await yt.pl(args[0].id), args });
+}
