10678547b0793a7720df0472bf671d291ed124ed
Ima Person <me@imaperson.dev>
2022-04-03 22:58:52 -0400

    Update new.js

--- socket/short/new.js
+++ socket/short/new.js
@@ -22,8 +22,6 @@ export default async (io, socket, args) => {
         await rateLimiter.consume(socket.handshake.address);
 
         if (!args[0] || !args[0].url || !args[0].domain || (["ml", "tk", "ga", "cf", "gq"].indexOf(args[0].domain) == -1) || !args[0].expires || isNaN(args[0].expires) || (args[0].expires > 2147483647) || ((args[0].expires - 600) < Math.round(new Date().getTime() / 1000))) return socket.emit("short-new", { success: false, message: "invalidargs", args });
-
-        if (args[0].domain == "tk") return socket.emit("short-new", { success: false, message: "invaliddomain", args });
         
         deleteExpired();
 
