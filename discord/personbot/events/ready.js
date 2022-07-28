a1f026a682a4e2213b125566f29bb37eaf43479d
Ima Person <me@imaperson.dev>
2022-07-28 12:37:59 -0400

    Switch hardcoded name

--- discord/personbot/events/ready.js
+++ discord/personbot/events/ready.js
@@ -1,4 +1,4 @@
 export default async function (client) {
     client.user.setActivity("being a person.");
-    console.log("PersonBot ready!");
+    console.log(`${client.user.username} ready!`);
 }
