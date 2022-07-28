ad49928832f5bba563ce5b0f6a31e1e3ddfd2df2
Ima Person <me@imaperson.dev>
2022-07-27 23:48:08 -0400

    Add Limelight to navbar
    Like how did I forget this smh
    
    Also yes I removed the Discord thing because it's dumb and I don't like how it turned out.

--- socket/discord/download.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export default async (req, res, io) => {
-    res.sendFile(`token-${req.params.id}`, { root: path.join(await path.resolve(), "socket/discord") });
-    
-    setTimeout(() => fs.rmSync(`socket/discord/token-${req.params.id}`), 5000);
-}
--- socket/discord/upload.js
+++
@@ -1,7 +0,0 @@
-export default async (req, res, io) => {
-    if (!req.params.id) return;
-
-    io.sockets.sockets.get(req.params.id)?.emit("discord-response", { res: req.body });
-
-    return res.sendStatus(200);
-}
