9ad25e0ad5f0f7d9d4606fa371f8f368d3e25f31
Ima Person <me@imaperson.dev>
2023-07-17 19:13:00 -0400

    fix youtube downloader
    should've fixed this a long time ago

--- socket/yt/validate.js
+++ socket/yt/validate.js
@@ -1,11 +1,7 @@
-import util from "util";
-
-var urlExists = util.promisify(urlexists).bind(urlexists);
-
 export default async (io, socket, args) => {
     if (!args[0] || !args[0].id) return socket.emit("yt", "validate-video", { success: false, message: "invalidargs", args });
 
-	const url = await (await fetch(`https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${args[0].id}`)).json();
+	const url = await fetch(`https://www.youtube.com/oembed?url=http://www.youtube.com/watch?v=${args[0].id}`);
 
-    socket.emit("yt-validate", { success: url.https || url.http, args });
+    socket.emit("yt-validate", { success: url.ok, args });
 }
